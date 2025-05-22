4b55d620795800be963020c631ac73e3104fcb7d
vickash <vickashmahabir@gmail.com>
2025-05-21 22:09:46 -0400

    Optimize Canvas#line

--- src/mrb_denko_fastcanvas.c
+++ src/mrb_denko_fastcanvas.c
@@ -3,6 +3,7 @@
 #include <mruby/hash.h>
 #include <mruby/variable.h>
 #include <mruby/value.h>
+#include <stdlib.h>
 
 // C struct to avoid constantly getting ivars.
 typedef struct {
@@ -31,6 +32,9 @@ mrb_get_canvas_data(mrb_state* mrb, mrb_value self, canvas_t* canvas) {
   canvas->swap_xy      = mrb_bool(mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "@swap_xy")));
 }
 
+//
+// #pixel
+//
 static void
 c_canvas_pixel(mrb_state* mrb, canvas_t* c, int x, int y, int color) {
   // Reverse current canvas transformations.
@@ -83,6 +87,96 @@ mrb_canvas_pixel(mrb_state* mrb, mrb_value self) {
   return mrb_nil_value();
 }
 
+//
+// #line
+//
+static void
+c_canvas_line(mrb_state* mrb, canvas_t* c, int x1, int y1, int x2, int y2, int color) {
+  // Deltas in each axis.
+  int dy = y2 - y1;
+  int dx = x2 - x1;
+
+  // Optimize vertical lines and avoid division by 0.
+  if (dx == 0) {
+    // Ensure y1 < y2.
+    if (y2 < y1) {
+      int t = y1;
+      y1 = y2;
+      y2 = t;
+    }
+    for(int y=y1; y<=y2; y++) {
+      c_canvas_pixel(mrb, c, x1, y, color);
+    }
+    return mrb_nil_value();
+  }
+
+  // Optimize horizontal lines.
+  if (dy == 0) {
+    // Ensure x1 < x2.
+    if (x2 < x1) {
+      int t = x1;
+      x1 = x2;
+      x2 = t;
+    }
+    for(int x=x1; x<=x2; x++) {
+      c_canvas_pixel(mrb, c, x, y1, color);
+    }
+    return mrb_nil_value();
+  }
+
+  // Bresenham's algorithm for sloped lines.
+  //
+  // Slope calculations
+  int dx_abs      = abs(dx);
+  int dy_abs      = abs(dy);
+  int step_axis   = (dx_abs > dy_abs) ? 0 : 1; // 0 = x, 1 = y
+  int step_count  = (step_axis == 0)  ? dx_abs : dy_abs;
+  int x_step      = (dx > 0) ? 1 : -1;
+  int y_step      = (dy > 0) ? 1 : -1;
+
+  // Error calculations
+  int error_step      = (step_axis == 0) ? dy_abs : dx_abs;
+  int error_threshold = (step_axis == 0) ? dx_abs : dy_abs;
+
+  int x = x1;
+  int y = y1;
+  int error = 0;
+  for (int i=0; i<=step_count; i++) {
+    c_canvas_pixel(mrb, c, x, y, color);
+
+    if (step_axis == 0) { // Step on x-axis
+      x += x_step;
+      error += error_step;
+      if (error >= error_threshold) {
+        y += y_step;
+        error -= error_threshold;
+      }
+    } else { // Step on y-axis
+      y += y_step;
+      error += error_step;
+      if (error >= error_threshold) {
+        x += x_step;
+        error -= error_threshold;
+      }
+    }
+  }
+}
+
+static mrb_value
+mrb_canvas_line(mrb_state* mrb, mrb_value self) {
+  mrb_int x1, y1, x2, y2;
+  mrb_value kwargs;
+  mrb_get_args(mrb, "iiiiH", &x1, &y1, &x2, &y2, &kwargs);
+
+  mrb_value mrb_color = mrb_hash_fetch(mrb, kwargs, mrb_symbol_value(mrb_intern_lit(mrb, "color")), mrb_fixnum_value(0));
+  mrb_int color = mrb_fixnum(mrb_color);
+
+  canvas_t canvas;
+  mrb_get_canvas_data(mrb, self, &canvas);
+
+  c_canvas_line(mrb, &canvas, x1, y1, x2, y2, color);
+  return mrb_nil_value();
+}
 
 void
 mrb_mruby_denko_fastcanvas_gem_init(mrb_state* mrb) {
@@ -97,6 +191,7 @@ mrb_mruby_denko_fastcanvas_gem_init(mrb_state* mrb) {
 
   // Optimized pixel methods for Canvas
   mrb_define_method(mrb, mrb_Canvas, "pixel",       mrb_canvas_pixel,       MRB_ARGS_REQ(3));
+  mrb_define_method(mrb, mrb_Canvas, "line",        mrb_canvas_line,        MRB_ARGS_REQ(4));
 }
 
 void
